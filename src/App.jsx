174209b78365e08608cf1b2e35f2b61951a730e7
Mushafiya <kmushafiya003@gmail.com>
2024-09-09 20:13:42 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
 import ProductDetails from './component/common/Productdetails'
+import Category from './component/common/Category'
 
 
 function App() {
 
 
   return (
-    <>
+  
       <div className='max-w-[100vh] min-h-screen overflow-x-hidden font-poppins'>
-<h1 className='text-red-100'>hi</h1>
+
       </div>
        
-    </>
+ 
   )
 }
 
