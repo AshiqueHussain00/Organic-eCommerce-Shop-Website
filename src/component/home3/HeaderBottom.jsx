949ca97d6e448d744ca9a7966c634e4adfd86cb2
pankaj-03 <pankajsunal045@gmail.com>
2024-09-09 13:54:40 +0530

    Mobile Design Basic Layout

--- src/component/home3/HeaderBottom.jsx
+++ src/component/home3/HeaderBottom.jsx
@@ -5,15 +5,15 @@ function HeaderBottom() {
   return (
     <section>
          <div className="w-10/12 mx-auto  bg-white-200 text-black-800">
-                 <ul className="flex items-center justify-center p-4 gap-5">
+                 <ul className="flex items-center flex-wrap justify-center p-4 gap-5">
                     {HeaderBottomData.map((item) => (
-                       <li key = {item.id} className="flex flex-row gap-3 p-2 hover:bg-primary hover:rounded-md  focus:text-white-200 cursor-pointer  group">
+                       <li key = {item.id} className="flex flex-col items-center justify-center xxl:flex-row gap-3 p-2 hover:bg-primary hover:rounded-md   focus:text-white-200 cursor-pointer  group">
                            <div>
-                                <img src = {item.image} style={{width: '45px', height:'45px'}} className="group-hover:bg-transparent"></img>
+                                <img src = {item.image} style={{width: '50px', height:'50px'}} className="group-hover:bg-transparent"></img>
                            </div>
 
-                           <div className="">
-                              <h4 className="text-gray-800 text-base font-semibold group-hover:text-white-200">{item.title}</h4>
+                           <div className=" text-center">
+                              <h4 className="text-gray-800 text-base item-center font-semibold group-hover:text-white-200">{item.title}</h4>
                               <p className="text-gray-400 text-sm font-normal group-hover:text-white-200 ">{item.description}</p>
                            </div>
                        </li>
