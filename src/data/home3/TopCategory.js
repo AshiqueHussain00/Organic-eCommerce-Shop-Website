da49a0310bd83991f6bb7cbb797593458f295601
pankaj-03 <pankajsunal045@gmail.com>
2024-09-10 20:49:22 +0530

    Data of Top Category

--- src/data/home3/TopCategory.js
+++ src/data/home3/TopCategory.js
@@ -1,6 +1,51 @@
+import image1 from '../../assets/home3/topCategory/Vegetable.svg'
+import image2 from '../../assets/home3/topCategory/Fruit.svg'
+import image3 from '../../assets/home3/topCategory/Fish.svg'
+import image4 from '../../assets/home3/topCategory/Meat.svg'
+import image5 from '../../assets/home3/topCategory/SoftDrink.svg'
+import image6 from '../../assets/home3/topCategory/Snacks.svg'
+
 export const TopCategoryData = [
     {
         id: 1,
-        
+        image: '',
+        title: 'Vegetables',
+        description: '165 Products'
+    },
+
+    {
+        id:2,
+        image: '',
+        title: 'Fresh Fruit',
+        description: '137 Products'
+    },
+
+    {
+        id:3 ,
+        image: '',
+        title: 'River Fish',
+        description: '34 Products'
+    },
+
+    {
+        id:4,
+        image: '',
+        title: 'Meat',
+        description:'165 Products'
+    },
+
+    {
+        id:5,
+        image: '',
+        title: 'Water and Drinks',
+        description: '48 Products'
+    },
+
+    {
+        id:6,
+        image: '',
+        title: 'Snacks',
+        description: '165 Products'
     }
+
 ]
