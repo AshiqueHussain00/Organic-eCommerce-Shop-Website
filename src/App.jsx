86f3b7ba1a7ccb5457a2af6f60af5f8b0ea37e6d
pankaj-03 <pankajsunal045@gmail.com>
2024-09-11 14:04:22 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import './App.css'
 
 import ShowProduct from './component/common/ShowProduct'
 import HeroBottom4 from './component/common/HeroBottom4'
+import Banner from './component/home3/Banner'
+import HomeOneHerosection from './component/home1/home-component/HomeOneHerosection'
 
 
 
@@ -14,7 +16,10 @@ function App() {
 
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
       {/* <ShowProduct/> */}
-    <HeroBottom4/>
+    {/* <HeroBottom4/>
+    <Banner/> */}
+    <HomeOneHerosection/>
+
 
 
     </div>
