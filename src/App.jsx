72ddc7003f8465eb306350a07ced99bca38733a6
Mushafiya <kmushafiya003@gmail.com>
2024-09-11 10:35:29 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import './App.css'
 
-import Home1 from './component/home1/Home1'
-
-import ShowProduct from './component/common/ShowProduct'
-import Home5 from './component/home5/Home5'
 
 
 
@@ -17,8 +13,7 @@ function App() {
 
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
       
-     <Home5/>
-
+  
     </div>
 
 
