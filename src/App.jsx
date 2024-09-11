2fa2d7e8230897e4381a26463ac35e02bec59f51
Mushafiya <kmushafiya003@gmail.com>
2024-09-11 10:33:40 +0530

    Product section for homepage4 completed with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 
 
 import HeroBottom4 from './component/common/HeroBottom4'
+import ProductSection from './component/home4/ProductSection'
+
 
 
 
@@ -12,7 +14,11 @@ function App() {
   return (
 
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
+
+
+
       {/* <ShowProduct/> */}
+      <ProductSection/>
     <HeroBottom4/>
 
 
