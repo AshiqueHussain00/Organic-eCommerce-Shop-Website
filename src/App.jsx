776465a08e6ec0d887dc3f13e7cb16101489a30e
Mushafiya <kmushafiya003@gmail.com>
2024-09-12 21:26:59 +0530

    Made Home4 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
 
 import './App.css'
-import ProductSection from './component/home4/ProductSection';
-import Banner from './component/home4/Banner';
-import FeaturedProduct from './component/home4/FeaturedProduct';
-import VideoSection from './component/home4/VideoSection'
-import LatestNewsSection from './component/home4/LatestNewsSection';
+
 import Home5 from './component/Home5/Home5';
+import Home4 from './component/page/homepage/home4';
 
 function App() {
   return (
     
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
 
-      <ProductSection/>
-      <Banner/>
-      <FeaturedProduct/>
-      <VideoSection/>
-    <LatestNewsSection/>
-
+     
+<Home4/>
 
     
   
---
+++ src/component/page/homepage/home4.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import HeroBottom4 from '../../common/HeroBottom4'
+import ProductSection from '../../home4/ProductSection'
+import Banner from '../../home4/Banner'
+import FeaturedProduct from '../../home4/FeaturedProduct'
+import VideoSection from '../../home4/VideoSection'
+import LatestNewsSection from '../../home4/LatestNewsSection'
+
+const Home4 = () => {
+  return (
+    <div>
+        <HeroBottom4/>
+        <ProductSection/>
+        <Banner/>
+        <FeaturedProduct/>
+        <VideoSection/>
+        <LatestNewsSection/>
+    </div>
+  )
+}
+
+export default Home4
