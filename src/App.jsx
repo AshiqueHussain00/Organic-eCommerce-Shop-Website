173df1a4cc22afabbdada5686375339615ab08db
Mushafiya <kmushafiya003@gmail.com>
2024-09-12 12:21:58 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
 import './App.css'
-import ProductSection from './component/home4/ProductSection';
-import Banner from './component/home4/Banner';
-import FeaturedProduct from './component/home4/FeaturedProduct';
-import VideoSection from './component/home4/VideoSection';
+// import ProductSection from './component/home4/ProductSection';
+// import Banner from './component/home4/Banner';
+// import FeaturedProduct from './component/home4/FeaturedProduct';
+// import VideoSection from './component/home4/VideoSection';
 
 
 
@@ -15,10 +15,7 @@ function App() {
 
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
 
-      <ProductSection/>
-      <Banner/>
-      <FeaturedProduct/>
-      <VideoSection/>
+      
     
   
     </div>
