d4fcae4b1f8300f3bbdc8e3943c0902706dd44aa
pankaj-03 <pankajsunal045@gmail.com>
2024-09-12 09:41:04 +0530

    Banner Date Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 
-
 import './App.css'
 import HeaderBottom from './component/home3/headerBottom'
 import TopCategory from './component/home3/TopCategory'
-import Home5 from './component/home5/Home5'
+import Home5 from './component/Home5/Home5'
+import Banner from './component/home3/Banner'
 
 
 
@@ -15,7 +15,10 @@ function App() {
 
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
     
-      <Home5/>
+      {/* <Home5/> */}
+      {/* <TopCategory/> */}
+      <Banner/>
+      
   
     </div>
   );
--- src/component/home3/TopCategory.jsx
+++ src/component/home3/TopCategory.jsx
@@ -116,3 +116,4 @@ function TopCategory() {
 export default TopCategory;
 
 
+
