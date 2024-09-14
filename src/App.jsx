93f45777d37181ff5109e7aaadafa659777ddc84
Mushafiya <kmushafiya003@gmail.com>
2024-09-14 12:09:08 +0530

    All category part of navbar completed for large screen

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home5 from './component/home5/Home5'
 import Home4 from './pages/homepages/Home4';
 // import Homepage2 from './component/home2/homepage2'
 import Home3 from './component/home3/Home3';
+import Navbar from './component/common/navbar';
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
@@ -28,8 +29,9 @@ const App = () => {
   return (
 
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
+      <Navbar/>
       {/* <Homepage2/> */}
-      <Home4/>
+      {/* <Home4/> */}
       {/* <Home1/> */}
       {/* <Home5/> */}
       {/* <Home3 /> */}
