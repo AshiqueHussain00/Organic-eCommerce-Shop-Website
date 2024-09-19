9f16bd7fd0a0150af39b9d5c754d9e701b4f541b
Mushafiya <kmushafiya003@gmail.com>
2024-09-19 22:11:08 +0530

    Styling of Cart Completed

--- src/component/cart & wishlist/SingleItem.jsx
+++ src/component/cart & wishlist/SingleItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { IoCloseCircleOutline } from "react-icons/io5";
-import { useDispatch , useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 // {
 //     id: "s1",
 //     name: "Tomato Ketchup",
@@ -73,9 +73,9 @@ import { useDispatch , useSelector } from 'react-redux';
 //         organic: "100% Organic",
 //     },
 // },
-const SingleItem = ({data}) => {
+const SingleItem = ({ data }) => {
 
-    
+
     const dispatch = useDispatch();
 
     const handleIncremenet = (id) => {
@@ -84,69 +84,101 @@ const SingleItem = ({data}) => {
     const handleDecremenet = (id) => {
 
     }
-    const handleDelete= (id) => {
+    const handleDelete = (id) => {
 
     }
 
 
 
 
-  return (
-    <div>
-    <div className='hidden xmd:grid grid-cols-6 border border-blue-600 py-4 font-normal text-gray-900 text-sm xl:text-[16px]'>
-
-        <div className='flex items-center gap-x-3  border  col-span-2'>
-            <div className='xlg:w-[100px] w-[80px]'>
-            <img src={data.images[0].main} alt={data.name} className='w-full h-full object-contain'/>
-            </div>
-          
-            <p className='tracking-wide'>{data.name}</p>
-        </div>
-
-        <div className='border flex items-center justify-center'>
-            $ {data.price.discounted}
-        </div>
-
-        <div className='flex border items-center justify-center gap-x-1 '>
-            <div className='flex items-center border py-1 px-1 xlg:px-2 gap-x-2 rounded-3xl'>
-            <div 
-            onClick={()=> handleDecremenet(data.id)}
-            className='border xlg:w-8 xlg:h-8 w-6 h-6 flex items-center justify-center rounded-full cursor-pointer text-lg bg-gray-50 transition-all duration-300  border-gray-50 hover:border-gray-900'>-</div>
-            <div>5</div>
-            <div 
-             onClick={()=> handleIncremenet(data.id)}
-            className=' xlg:w-8 xlg:h-8 w-6 h-6 flex items-center justify-center rounded-full text-lg bg-gray-50 cursor-pointer transition-all duration-300 border border-gray-50 hover:border-gray-900'>+</div>
-            </div>
-          
-        </div>
-
-        <div className='border flex items-center justify-center'>
-           {/* {data.totalPrice} */}
-           $1400
-        </div>
-
-        <div 
-         onClick={()=> handleDelete(data.id)}
-        className='border text-xl xlg:text-[28px]  flex items-center justify-center cursor-pointer '>
-            <IoCloseCircleOutline className='text-gray-700 transition-all duration-300 hover:text-gray-400'/>
-        </div>
-             
-    </div>
-
-    {/* ------------- for small screen -------------- */}
-
-    <div className='xmd:hidden'>
-{/* 
+    return (
         <div>
-            <img src={data.images[0].main} alt={data.name} />
-        </div> */}
+            <div className='hidden xs:grid md:hidden xmd:grid grid-cols-6 border py-4 font-normal text-gray-900 text-sm xl:text-[16px]'>
 
-    </div>
+                <div className='flex items-center gap-x-3    col-span-2'>
+                    <div className='xlg:w-[100px] w-[80px]'>
+                        <img src={data.images[0].main} alt={data.name} className='w-full h-full object-contain' />
+                    </div>
+
+                    <p className='tracking-wide'>{data.name}</p>
+                </div>
+
+                <div className=' flex items-center justify-center'>
+                    $ {data.price.discounted}
+                </div>
+
+                <div className='flex  items-center justify-center gap-x-1 '>
+                    <div className='flex items-center border py-1 px-1 xlg:px-2 gap-x-2 rounded-3xl'>
+                        <div
+                            onClick={() => handleDecremenet(data.id)}
+                            className='border xlg:w-8 xlg:h-8 w-6 h-6 flex items-center justify-center rounded-full cursor-pointer text-lg bg-gray-50 transition-all duration-300  border-gray-50 hover:border-gray-900'>-</div>
+                        <div>5</div>
+                        <div
+                            onClick={() => handleIncremenet(data.id)}
+                            className=' xlg:w-8 xlg:h-8 w-6 h-6 flex items-center justify-center rounded-full text-lg bg-gray-50 cursor-pointer transition-all duration-300 border border-gray-50 hover:border-gray-900'>+</div>
+                    </div>
+
+                </div>
+
+                <div className=' flex items-center justify-center'>
+                    {/* {data.totalPrice} */}
+                    $1400
+                </div>
+
+                <div
+                    onClick={() => handleDelete(data.id)}
+                    className='text-xl xlg:text-[28px]  flex items-center justify-center cursor-pointer '>
+                    <IoCloseCircleOutline className='text-gray-700 transition-all duration-300 hover:text-gray-400' />
+                </div>
+
+            </div>
+
+            {/* ------------- for small screen -------------- */}
+
+            <div className='xmd:hidden xs:hidden grid md:grid grid-cols-4 border- xsm:text-[16px] text-sm border'>
+
+                <div className=' col-span-3 py-4 flex items-center gap-x-5'>
+
+                    <div className='xsm:w-[100px] w-[80px]'>
+                        <img src={data.images[0].main} alt={data.name} className='w-full h-full object-contain' />
+                    </div>
+
+                    <div className='flex flex-col gap-y-3'>
+
+                    <p className='tracking-wide'>{data.name}</p>
+                        <p className=''>Price: <span className='text-[16px]'>$ {data.price.discounted}</span></p>
+
+                        <div className='flex  items-center  gap-x-1 '>
+
+                            <div className='flex items-center border py-1 px-2 gap-x-2 rounded-3xl '>
+                                <div
+                                    onClick={() => handleDecremenet(data.id)}
+                                    className='border xs:w-8 xs:h-8 w-6 h-6 flex items-center justify-center rounded-full cursor-pointer text-lg bg-gray-50 transition-all duration-300  border-gray-50 hover:border-gray-900'>-</div>
+                                <div>5</div>
+                                <div
+                                    onClick={() => handleIncremenet(data.id)}
+                                    className=' xs:w-8 xs:h-8 w-6 h-6 flex items-center justify-center rounded-full text-lg bg-gray-50 cursor-pointer transition-all duration-300 border border-gray-50 hover:border-gray-900'>+</div>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div className=' flex items-center justify-center'>
+                    {/* {data.totalPrice} */}
+                    $1400
+                </div>
 
 
 
-    </div>
-  )
+            </div>
+
+
+
+        </div>
+    )
 }
 
 export default SingleItem
