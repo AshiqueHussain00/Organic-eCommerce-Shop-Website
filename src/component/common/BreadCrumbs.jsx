501e43a05c0a663fccefdc88e6f907a7155cedc7
pankaj-03 <pankajsunal045@gmail.com>
2024-09-20 16:46:28 +0530

    BreadCrumb Layout

--- src/component/common/BreadCrumbs.jsx
+++ src/component/common/BreadCrumbs.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Link , useLocation} from 'react-router-dom';
+import { PiGreaterThanBold } from 'react-icons/pi';
+import { GoHome } from 'react-icons/go';
 
 function BreadCrumbs() {
 
@@ -9,13 +11,16 @@ function BreadCrumbs() {
     console.log(pathnames);
   return (
     <section>
-      <Link to="/" >Home</Link>
+        <div className=" w-10/12 p-2 flex">
+        <Link to="/" ><GoHome className="h-[20px] w-[20px] mr-1"/></Link>
       {pathnames.map((name , index)=>{
         breadcrumbPath += `${name}`;
         const isLast = index === pathnames.length - 1;
 
-        return isLast? <span key = {breadcrumbPath}> {' > '} {name}</span> : (<span key={breadcrumbPath}>{' > '}<Link to = {breadcrumbPath}>{name}</Link></span>)
+        return isLast? <span key = {breadcrumbPath} className="flex items-center text-green-600"><PiGreaterThanBold className="mx-2 text-gray-700"/> {name}</span> : (<span key={breadcrumbPath} className="flex items-center text-gray-700"><PiGreaterThanBold  className="mx-2 text-gray-700"/><Link to = {breadcrumbPath}>{name}</Link></span>)
       })}
+        </div>
+      
     </section>
   )
 }
