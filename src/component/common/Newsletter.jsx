4c1e690e88bf8c3e4c5973f567ac78b49a96f03d
Mushafiya <kmushafiya003@gmail.com>
2024-09-23 12:05:58 +0530

    Added Newsletter to home4

--- src/component/common/Newsletter.jsx
+++ src/component/common/Newsletter.jsx
@@ -2,10 +2,16 @@
 import React from 'react';
 import logo from '../../../public/Logo.svg'
 
-const Newsletter = () => {
+const Newsletter = ({isHome4 = false}) => {
     return (
-        <div className="py-8 bg-gray-100">
-            <div className="flex flex-col items-center justify-between px-4 mx-auto max-w-7xl sm:px-6 lg:px-8 lg:flex-row">
+        <div className={` py-8   w-full
+            ${
+                isHome4 ? " bg-green-gray-scale-50" : "bg-gray-100"
+            }
+        `}>
+            <div className={` flex flex-col items-center gap-x-6 justify-between px-4 mx-auto  sm:px-6  lg:flex-row  max-w-7xl lg:px-8
+               
+                `}>
                 {/* Logo Section */}
                 <div className="flex items-center mb-6 lg:mb-0">
                     <img src={logo} alt="Ecobazar Logo" className="" />
@@ -13,11 +19,11 @@ const Newsletter = () => {
                 </div>
 
                 {/* Newsletter Section */}
-                <div className="flex flex-col items-center lg:flex-row lg:items-start">
+                <div className="flex flex-col items-center lg:flex-row ">
                     {/* Text Section */}
-                    <div className="mb-4 lg:mb-0 lg:mr-8">
-                        <h2 className="text-lg font-semibold text-gray-900">Subscribe to our Newsletter</h2>
-                        <p className="text-gray-500">Pellentesque eu nibh eget mauris congue mattis mattis.</p>
+                    <div className="mb-4 lg:mb-0 lg:mr-8 ">
+                        <h2 className="text-lg font-semibold text-gray-900 text-center xl:text-left">Subscribe to our Newsletter</h2>
+                        <p className="text-gray-500 text-center">Pellentesque eu nibh eget mauris congue mattis mattis.</p>
                     </div>
 
                     {/* Input and Button Section */}
