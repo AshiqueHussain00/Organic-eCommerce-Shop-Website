1c42709cca199a2e07fbbc073a1e0d2b808c4382
nalini jadhav <nalini@edulab.in>
2024-09-26 18:22:32 +0530

    member aos

--- src/component/home2/Members.jsx
+++ src/component/home2/Members.jsx
@@ -83,7 +83,7 @@ const Card = ({ title, image, description }) => {
 const Members = () => {
   return (
     <section className="flex items-center justify-center w-full">
-      <div className="w-11/12 xmd:w-10/12 mt-8 mx-auto xs:max-h-[500px]">
+      <div data-aos="fade-left"className="w-11/12 xmd:w-10/12 mt-8 mx-auto xs:max-h-[500px]">
         <div className="mt-12 mb-6 tracking-wider text-center text-primary">
           <p>TEAM</p>
           <h2 className="mb-2 text-3xl font-semibold text-black-900">
