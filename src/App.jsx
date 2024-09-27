38d567c19f75b89f811b28d843c408098beb2e3e
pankaj-03 <pankajsunal045@gmail.com>
2024-09-27 21:59:53 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -111,9 +111,6 @@ const App = () => {
   return (
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
       <Navbar />
-      {/* <Setting/> */}
-
-
       <Suspense fallback={<Loader />}>
         <Routes>
           {/* ---------- homepages ------- */}
