138678563ad2a7f0e0c98726778adab94fc6a55d
Ashique <ansariazad7864@gmail.com>
2024-09-28 22:50:02 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -113,8 +113,8 @@ const App = () => {
 
   return (
     <div className='max-w-[100vw] min-h-screen overflow-x-hidden font-poppins'>
-      {/* <Navbar /> */}
-      <Footer4/>
+      <Navbar />
+      
       <Suspense fallback={<Loader />}>
         <Routes>
           {/* ---------- homepages ------- */}
