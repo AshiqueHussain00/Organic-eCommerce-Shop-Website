ef28ec60960f93b564e6a2072141864bac6eaa7a
akki9689 <akashpuri259@gmail.com>
2024-09-30 10:56:08 +0530

    ...

--- src/component/common/popup/PopupNewsletter.jsx
+++ src/component/common/popup/PopupNewsletter.jsx
@@ -6,11 +6,10 @@ const PopupNewsletter = () => {
   const [email, setEmail] = useState('');
   const [dontShow, setDontShow] = useState(false);
 
-  // Check localStorage on component mount
+  // Check sessionStorage on component mount
   useEffect(() => {
-    const dontShowAgain = localStorage.getItem('dontShowNewsletter');
+    const dontShowAgain = sessionStorage.getItem('dontShowNewsletter');
     if (!dontShowAgain) {
-      // Show the popup immediately
       setShowPopup(true);
     }
   }, []);
@@ -27,7 +26,7 @@ const PopupNewsletter = () => {
 
   const closePopup = () => {
     if (dontShow) {
-      localStorage.setItem('dontShowNewsletter', 'true'); // Store preference in localStorage
+      sessionStorage.setItem('dontShowNewsletter', 'true');
     }
     setShowPopup(false);
   };
@@ -35,27 +34,27 @@ const PopupNewsletter = () => {
   return (
     <>
       {showPopup && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black-900 bg-opacity-80 p-4">
-          <div className=" p-2 relative bg-white-200 rounded-lg overflow-hidden flex flex-col md:flex-row w-full max-w-4xl h-auto md:h-[40vh]">
+        <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black-900 bg-opacity-90 p-4">
+          <div className="p-2 relative bg-white-200 rounded-lg overflow-hidden flex flex-col md:flex-row w-full max-w-4xl h-auto md:h-[40vh]">
             
             {/* Close Button */}
             <button
               onClick={closePopup}
-              className="absolute top-2 right-2 bg-white text-gray-800 rounded-full p-2 shadow hover:bg-gray-200 focus:outline-none"
+              className="absolute top-10 right-2 bg-white-100 text-gray-800 rounded-full p-2 shadow hover:bg-gray-200 focus:outline-none"
             >
               ✕
             </button>
 
             {/* Left Side: Background Image */}
             <div
-              className="w-full  bg-contain mx-auto md:w-1/3 h-[200px] md:h-full md:bg-cover bg-center bg-no-repeat"
+              className="w-full bg-contain mx-auto md:w-1/3 h-[200px] md:h-full md:bg-cover bg-center bg-no-repeat"
               style={{ backgroundImage: `url(${BG})` }}
             ></div>
 
             {/* Right Side: Content */}
             <div className="w-full md:w-2/3 p-6 md:p-8 flex flex-col justify-center items-center">
               <h1 className="text-2xl md:text-4xl font-bold mb-4 text-center">Subscribe to our Newsletter</h1>
-              <p className=" text-gray-400 text-center mb-6 text-sm md:text-base">
+              <p className="text-gray-400 text-center mb-6 text-sm md:text-base">
                 Subscribe to our newsletter and save your <span className="text-[rgb(217,135,68)] font-bold">20%</span> <span className='text-[rgb(217,135,68)] font-bold'> money</span> with a discount code today.
               </p>
 
